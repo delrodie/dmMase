0d5230704144b2364fbc40f454335ec8ef711186
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-12-28 18:26:24 +0000

    gestion des actualites

---
+++ src/Listerner/ActualiteListerner.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Listerner;
+
+
+use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
+
+class ActualiteListerner implements EventSubscriberInterface
+{
+
+    public function getSubscribedEvents()
+    {
+        // TODO: Implement getSubscribedEvents() method.
+    }
+}
