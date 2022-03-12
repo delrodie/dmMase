d747751e8a1b39d9b141759e28d579ec63b10735
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2022-03-12 01:15:28 +0000

    Frontend Entreprise EI

---
+++ src/Controller/FrAdhesionEIController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Entreprise;
+use App\Form\EntrepriseType;
+use App\Repository\AgendaRepository;
+use App\Repository\EntrepriseRepository;
+use App\Utilities\GestionMedia;
+use Cocur\Slugify\Slugify;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/adhesion/entreprises-intervenantes")
+ */
+class FrAdhesionEIController extends AbstractController
+{
+	private $agendaRepository;
+	private $gestionMedia;
+	private $entrepriseRepository;
+	
+	public function __construct(AgendaRepository $agendaRepository, GestionMedia $gestionMedia, EntrepriseRepository $entrepriseRepository)
+	{
+		$this->agendaRepository = $agendaRepository;
+		$this->gestionMedia = $gestionMedia;
+		$this->entrepriseRepository = $entrepriseRepository;
+	}
+	
+    /**
+     * @Route("/", name="frontend_adhesion_ei_formulaire", methods={"GET","POST"})
+     */
+    public function index(Request $request): Response
+    {
+	    $entreprise = new Entreprise();
+	    $form = $this->createForm(EntrepriseType::class, $entreprise);
+	    $form->handleRequest($request);
+	
+	    if ($form->isSubmitted() && $form->isValid()) {
+		    $entityManager = $this->getDoctrine()->getManager();
+		
+		    // Gestion des medias
+		    $mediaFile = $form->get('logo')->getData();
+		
+		    if ($mediaFile){
+			    $media = $this->gestionMedia->upload($mediaFile, 'entreprise');
+			
+			    $entreprise->setLogo($media);
+		    }else{
+			    $this->addFlash('danger', "<strong>Erreur!</strong> Vous devez télécharger votre logo");
+			
+			    return $this->redirectToRoute('frontend_adhesion_ei_formulaire');
+		    }
+		
+		    //gestion des slug
+		    $slugify = new Slugify();
+		    $slug = $slugify->slugify($entreprise->getRaisonSociale());
+		    $entreprise->setSlug($slug);
+		
+		    $entreprise->setType('EI');
+		
+		    $entityManager->persist($entreprise);
+		    $entityManager->flush();
+		
+		    return $this->redirectToRoute('frontend_adhesion_ei_telechargement',[
+			    'slug' => $entreprise->getSlug()
+		    ]);
+	    }
+		
+        return $this->render('fr_adhesion_ei/index.html.twig', [
+	        'agendas' => $this->agendaRepository->getEncours(),
+	        'form' => $form->createView(),
+        ]);
+    }
+	
+	/**
+	 * @Route("/{slug}", name="frontend_adhesion_ei_telechargement", methods={"GET"})
+	 */
+	public function telechargement(Entreprise $entreprise)
+	{
+		return $this->render('fr_adhesion_ei/telechargement.html.twig',[
+			'agendas' => $this->agendaRepository->getEncours(),
+			'entreprise' => $entreprise
+		]);
+	}
+}
