a2886068d8edd22232fbb2d2a81335921930d821
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2022-03-22 15:16:11 +0000

    Analytics 1

--- src/Controller/DashbordController.php
+++ src/Controller/DashbordController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use App\Utilities\Analytics;
+use Google\Analytics\Data\V1beta\BetaAnalyticsDataClient;
+use Google\Analytics\Data\V1beta\DateRange;
+use Google\Analytics\Data\V1beta\Dimension;
+use Google\Analytics\Data\V1beta\Metric;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class DashbordController extends AbstractController
 {
-	
+	CONST GOOGLE_APPLICATION_CREDENTIALS="/home/delrodie/www/backoffice/dreammaker/mase/public/analytics/DM-Mase-bb67a4531e94.json";
 	private $analytics;
 	
 	public function __construct(Analytics $analytics)
@@ -25,6 +29,31 @@ class DashbordController extends AbstractController
 	 */
     public function index(): Response
     {
+		putenv('GOOGLE_APPLICATION_CREDENTIALS='.$this->analytics->maseGA4());
+		$property_id = '307925646';
+		$client = new BetaAnalyticsDataClient(); //dd($client);
+		
+		$response = $client->runReport([
+			'property' => 'properties/'.$property_id,
+			'dateRanges' => [
+				new DateRange([
+					'start_date' => '2021-01-01',
+					'end_date' => 'today'
+				])
+			],
+			'dimensions' => [new Dimension(
+				['name' => 'city']
+			)]
+		]);
+		
+		print 'Report result: '. PHP_EOL;
+		
+		foreach ($response->getRows() as $row){ //dd($row);
+			$resultats =  $row->getDimensionValues()[0]->getValue()
+				. ' '.$row->getMetricsValues()[0]->getValue() . PHP_EOL;
+		}
+		
+		dd($response->getRowCount());
         return $this->render('dashbord/index.html.twig', [
             'aujourdhui' => date('Y-m-d', time()),
         ]);
--- src/Utilities/Analytics.php
+++ src/Utilities/Analytics.php
@@ -5,14 +5,21 @@
 	class Analytics
 	{
 		private $fichierDir;
+		private $maseDir;
 		
-		public function __construct($fichierDir)
+		public function __construct($fichierDir, $maseDir)
 		{
 			$this->fichierDir = $fichierDir;
+			$this->maseDir = $maseDir;
 		}
 		
-		public function fichier()
+		public function fichierUA()
 		{
 			return $this->fichierDir;
+		}
+		
+		public function maseGA4()
+		{
+			return $this->maseDir;
 		}
 	}
